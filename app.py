3d27e0b55a53195b2bd7ec6d46e06e4f461ecc25
Bakr <b.annour@stuart.com>
2021-11-01 17:56:06 +0000

    Final commit

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+USERNAME = "<INSERT YOUR NAME HERE>"
 
 @app.route('/')
 def hello_world():
+    #Return simple string for home page
     return 'Hello World'
 
 @app.route('/index')
 def homepage():
-    return app.send_static_file("index.html")
+    #Render html page with value set for the user variable
+    return render_template("index.html", user = USERNAME)
 
 if __name__ == '__main__':
     #Run app on port 5000 in debug mode. Host is specified as Flask needs you to give a host for apps that would
